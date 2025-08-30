8eb9b35d2dc8def65a024c7286677b86d0790d09
simpleAmazon <aakashtripathi54@gmail.com>
2025-08-30 18:49:08 +0530

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,55 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/backend-practice.js';
 //import '../data/cart-class.js';
 
-loadProducts(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve("value2");
+    });
+  })
+
+]).then((values) => {
+    console.log(values);
+    renderOrderSummary();
+    renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve("value1");
+  });
+  // resolve lets us control when to move to the next step
+  // whatever we give to resolve it will save inside the value variable in the next then
+})
+  .then((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+}); */
+
+// promises are the better way to handle async code instead of callback hell
