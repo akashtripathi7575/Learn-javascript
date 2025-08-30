0f753caadd12f2c6c58c7f4fe830e437169499ec
simpleAmazon <aakashtripathi54@gmail.com>
2025-08-30 20:00:45 +0530

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,24 @@ import { loadCart } from "../data/cart.js";
 //import '../data/backend-practice.js';
 //import '../data/cart-class.js';
 
+async function loadPage() {
+  
+
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+  renderOrderSummary();
+  renderPaymentSummary();
+
+}
+loadPage();
+
+//await lets us wait for a promise to resolve inside an async function before going to the next step
+/*
 Promise.all([
   loadProductsFetch(), // returns a promise directly
   new Promise((resolve) => {
@@ -15,10 +33,9 @@ Promise.all([
 
 ]).then((values) => {
     console.log(values);
-    renderOrderSummary();
-    renderPaymentSummary();
+ 
 });
-
+*/
 /*
 new Promise((resolve) => {
   loadProducts(() => {
@@ -49,3 +66,9 @@ loadProducts(() => {
 }); */
 
 // promises are the better way to handle async code instead of callback hell
+
+// async await is the better way to handle promises
+
+// fetch is the better way to handle http requests instead of XHR
+// fetch always returns a promise
+// async await is the shortcut for promises and lets us write async code like a normal code(sync code)
